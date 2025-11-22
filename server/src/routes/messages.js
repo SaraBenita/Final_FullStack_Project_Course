e39442eb18ebb80866f6118f02f54c9eca3b8b28
saraBenita <sara63101@gmail.com>
2025-11-22 23:54:15 +0200

    new update

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -6,7 +6,7 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 
-const uploadDir = path.join(process.cwd(), 'server', 'uploads');
+const uploadDir = path.join(process.cwd(), 'uploads');
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
 
 const storage = multer.diskStorage({
